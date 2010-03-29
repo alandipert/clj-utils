ee0a2aa4d542d7c4c8bb98f89eda94d679e4bf71
Alan Dipert <alan.dipert@gmail.com>
2010-03-29 02:24:37 -0400

    learned mapcat!

--- src/org/dipert/utils.clj
+++ src/org/dipert/utils.clj
@@ -1,17 +1,29 @@
-(ns org.dipert.utils.math
+(ns org.dipert.utils
   "These are some math-related functions I've found useful. 8-)"
   (:use (clojure.contrib math)))
 
-(defn- powers-of [n]
+(defn powers-of [n]
   "For given base n, create lazy sequence of powers of n"
   (iterate #(* % n) 1))
 
-(defn- char-range [& limits]
-  "Given an even number of limit characters, 
+(defn char-range [& limits]
+  {:pre [(even? (count limits))]}
+  "Given an even number of limit characters,
    creates an inclusive character sequence"
-  (apply concat (map (fn [[start end]]
-		       (map char (range (int start)
-					(inc (int end))))) (partition 2 limits))))
+  (map char (mapcat (fn [[start end]]
+		      (range (int start)
+			     (inc (int end))))
+		    (partition 2 limits))))
+
+(defn rands-from [from-set]
+  "Lazy sequence of random members of from-set"
+  (let [from (vec from-set)]
+    (lazy-seq (cons (from (rand-int (count from))) (rands-from from)))))
+
+(defn rand-str [len from-chars]
+  "Random string of length len composed of items
+   from from-chars"
+  (apply str (take len (rands-from from-chars))))
 
 (defn- syms-vals
   "Maps a base's representation characters to 
@@ -40,3 +52,8 @@
   [& args]
   {:pre [(odd? (count args))]}
   `(and ~@(map (fn [[n1 f n2]] `(~f ~n1 ~n2)) (partition 3 2 args))))
+
+(defmacro with-private-vars [[ns vars] & tests]
+  "Refers private fns from ns and runs tests in context."
+  `(let ~(reduce #(conj %1 %2 `@(ns-resolve '~ns '~%2)) [] vars)
+     ~@tests))
--- test/org/dipert/test/utils.clj
+++ test/org/dipert/test/utils.clj
@@ -1,21 +1,8 @@
-(ns org.dipert.utils.test.math
-  (:use [org.dipert.utils.math])
-  (:use [clojure.test]))
+(ns org.dipert.test.utils
+  (:use (clojure.test)
+	(org.dipert.utils)))
 
-(defmacro with-private-vars [[ns vars] & tests]
-  "Refers private fns from ns and runs tests in context."
-  `(let ~(reduce #(conj %1 %2 `@(ns-resolve '~ns '~%2)) [] vars)
-     ~@tests))
-
-(with-private-vars [org.dipert.utils.math [powers-of syms-vals char-range prep-str]]
-
-  (deftest test-powers-of
-    (let [powers-2 (powers-of 2)
-	  powers-8 (powers-of 8)
-	  powers-10 (powers-of 10)]
-      (is (= 1024 (nth powers-2 10)) "The 10th power of 2 should be 1024")
-      (is (= 73 (reduce + (take 3 powers-8))) "The sum of the first 3 powers of 8 should be 73")
-      (is (= 100000 (nth powers-10 5)))) "The sum of the first 5 powers of 10 should be 100000")
+(with-private-vars [org.dipert.utils [syms-vals char-range prep-str]]
 
   (deftest test-syms-vals
     (let [base-10-syms (syms-vals 10)]
@@ -32,6 +19,14 @@
     (is (= 1 (last (prep-str "10c" 16))) "The last value of the hex string '10c' should be 1 (1=1)")
     (is (= 3 (count (prep-str "azbxc" 16))) "Non-hex chars in input should be ignored")))
 
+(deftest test-powers-of
+  (let [powers-2 (powers-of 2)
+	powers-8 (powers-of 8)
+	powers-10 (powers-of 10)]
+    (is (= 1024 (nth powers-2 10)) "The 10th power of 2 should be 1024")
+    (is (= 73 (reduce + (take 3 powers-8))) "The sum of the first 3 powers of 8 should be 73")
+    (is (= 100000 (nth powers-10 5)))) "The sum of the first 5 powers of 10 should be 100000")
+
 (deftest test-strton
   (is (= 3764 (strton "0111010110100" 2)) "The binary string should equal 3764 in decimal")
   (is (= 3764 (strton "7264" 8)) "The octal string should equal 3764 in decimal")
@@ -46,3 +41,22 @@
   (is (= false (ccmp 1 < 0)) "One should not be less than zero")
   (is (= false (ccmp 1 >= 0 < 10 > 200)) "Bonkers to expect this to work")
   (is (= true (ccmp 5 < 10 = 10)) "The equality operator is in the mix"))
+
+(deftest test-char-range
+  (is (= [\A \B \C \0 \1 \2] (char-range \A \C \0 \2)))
+  (is (= (map #(char (+ 97 %)) (range 0 26)) (char-range \a \z))))
+
+(deftest test-rands-from
+  (let [from-set #{:A :B :C}]
+    (is (= 100 (count (take 100 (rands-from from-set)))))
+    (is (= nil ((set (take 10 (rands-from from-set))) :D)))))
+
+(deftest test-rand-str
+  (let [randstr (rand-str 10 (char-range \a \z \A \Z))]
+    (is (= 10 (count randstr)))
+    (is (every? char? randstr))
+    (is (not-any? #(Character/isWhitespace %)
+		  randstr))
+    (is (every? #(and (>= (int %) (int \A))
+		      (<= (int %) (int \z)))
+		randstr))))
