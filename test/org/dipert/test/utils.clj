6cf5547277c7f0b69079bdb5ae10ae1df4d2d2a6
Alan Dipert <alan.dipert@gmail.com>
2010-03-29 04:07:53 -0400

    testcleaning. lein test still brokt :\

--- test/org/dipert/test/utils.clj
+++ test/org/dipert/test/utils.clj
@@ -9,18 +9,18 @@
       (are [x y] (= x y)
 	   10 (count base-10-syms)
 	   2  (base-10-syms \2)
-	   45 (reduce + (vals base-10-syms)))
+	   45 (reduce + (vals base-10-syms)) "Powers of 10 should have these properties")
       (are [x] (thrown? AssertionError x)
 	   (syms-vals 123)
 	   (syms-vals 0)
-	   (syms-vals -34))))
+	   (syms-vals -34)) "Should throw AssertionErrors"))
   
   (deftest test-prep-str
     (are [x y] (= x y)
 	 3  (count (prep-str "abc" 16))
 	 12 (first (prep-str "10c" 16))
-	 1  (last (prep-str "10c" 16))
-	 3  (count (prep-str "azbxc" 16)))))
+	 1  (last  (prep-str "10c" 16))
+	 3  (count (prep-str "azbxc" 16))) "Should match the correct representation strings"))
 
 (deftest test-powers-of
   (let [powers-2 (powers-of 2)
@@ -29,7 +29,7 @@
     (are [x y] (= x y)
 	 1024   (nth powers-2 10)
 	 73     (reduce + (take 3 powers-8))
-	 100000 (nth powers-10 5))))
+	 100000 (nth powers-10 5)) "Powers of 2,8,10 should have these properties"))
 
 (deftest test-strton
   (are [x y] (= x y)
@@ -39,14 +39,14 @@
        3764 (strton "eb4" 16)
        3764 (strton "zeb4q" 16)
        3764 (strton "0xEB4" 16)
-       717  (strton "5a2" 11)))
+       717  (strton "5a2" 11) "Should all be equal to these base-10 numbers"))
 
 (deftest test-ccmp
   (are [x y] (= x y)
        true  (ccmp 1 < 2 < 3 < 100 <= 433)
        false (ccmp 1 < 0)
        false (ccmp 1 >= 0 < 10 > 200)
-       true  (ccmp 5 < 10 = 10)))
+       true  (ccmp 5 < 10 = 10) "Should result in all of these booleans"))
 
 (deftest test-char-range
   (is (= [\A \B \C \0 \1 \2] (char-range \A \C \0 \2)))
@@ -56,14 +56,14 @@
   (let [from-set #{:A :B :C}]
     (are [x y] (= x y)
 	 100 (count (take 100 (rands-from from-set)))
-	 nil ((set (take 10 (rands-from from-set))) :D))))
+	 nil ((set (take 10 (rands-from from-set))) :D)) "Random members of the set should satisfy these properties"))
 
 (deftest test-rand-str
   (let [randstr (rand-str 10 (char-range \a \z \A \Z))]
-    (is (= 10 (count randstr)))
-    (is (every? char? randstr))
+    (is (= 10 (count randstr)) "Should be 10 characters long")
+    (is (every? char? randstr) "Every member should be a char")
     (is (not-any? #(Character/isWhitespace %)
-		  randstr))
+		  randstr) "There should be no whitespace")
     (is (every? #(and (>= (int %) (int \A))
 		      (<= (int %) (int \z)))
-		randstr))))
+		randstr)) "Every member should be greater than A and less than z"))
