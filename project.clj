596c0d6b7c4f343c2b00816b4981b0a7b25550e2
Alan Dipert <alan.dipert@gmail.com>
2010-06-16 00:13:25 -0400

    upgrade deps

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject alan-utils "1.0.0-SNAPSHOT"
   :description "alan's clj stuff"
-  :dependencies [[org.clojure/clojure "1.1.0"]
-                 [org.clojure/clojure-contrib "1.1.0"]]
-  :dev-dependencies [[leiningen/lein-swank "1.1.0"]])
+  :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
+                 [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]]
+  :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]
+                     [swank-clojure "1.2.1"]])
